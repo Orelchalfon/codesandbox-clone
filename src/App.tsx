67cf97e893e96823496e796b912d756e8ce7b742
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-01-12 01:24:19 +0200

    fixing overflow on the x axis for better ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Branches } from "./Components/Branches";
 import Collaboration from "./Components/Collaboration";
 import Hero from "./Components/Hero";
+import { Branches } from "./Components/Branches";
 
 function App() {
   return (
-    <div className={`bg-background text-text`}>
+    <div className={`bg-background text-text `}>
       <main>
         <Hero />
-        <Collaboration />
-        <Branches />
+        <div className="relative z-10 w-full overflow-x-clip">
+          <Collaboration />
+          <Branches />
+        </div>
       </main>
     </div>
   )
